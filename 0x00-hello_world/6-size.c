b738e73e748c448cd8722aa4cf2136ef6a596010
Jonathan Bangala <jbangala90@gmail.com>
2023-02-16 06:59:56 -0800

    Task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main -Print out sizes of data types in C
+ * code by me
+ * Return: 0
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+}
